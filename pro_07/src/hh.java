44797d6203a298a8db9196171c817c62f72c8e99
Haiyan <guohy1215@163.com>
2022-07-02 20:53:34 +0800

    first commit

---
+++ pro_07/src/hh.java
@@ -0,0 +1,15 @@
+import java.io.FileReader;
+import java.io.Reader;
+
+public class hh {
+    static int age;
+    public static void main(String[] args) {
+        age=age+1;
+        System.out.println(age);
+    }
+
+
+    <String,Test,Java> String get(String string,Test t){
+        return string;
+    }
+}
